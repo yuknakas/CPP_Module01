31665b3b4869dd8f04920afef5ef2a95d70da328
yuknakas <yukinakashiba@gmail.com>
2026-01-02 16:47:00 +0900

    testing ex02

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yuknakas <yuknakas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/31 20:19:16 by yuknakas          #+#    #+#             */
-/*   Updated: 2026/01/01 10:21:23 by yuknakas         ###   ########.fr       */
+/*   Updated: 2026/01/02 16:41:25 by yuknakas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 int	main( void )
 {
-	std::string	str = "HI THIS IS BRAIN";
+	// declaring variables
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+
+	// printing memeory address
+	std::cout << "Memory address of string:    " << std::hex << &string << std::endl;
+	std::cout << "Memory address of pointer:   " << std::hex << &stringPTR << std::endl;
+	std::cout << "Memory address of reference: " << std::hex << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	// printing vale
+	std::cout << "Value of string:    " << string << std::endl;
+	std::cout << "Value of pointer:   " << stringPTR << std::endl;
+	std::cout << "Value of reference: " << stringREF << std::endl;
+
+	return (0);
 }
